039f97af42a70ca4ae5f16b8b9caa98cfcca345f
sebabrata <sebabratakundu9@gmail.com>
2021-10-28 19:25:10 +0530

    learnt about interpolation, reactive value, reactive statement, event binding, data binding, two way binding

---
+++ src/App.svelte
@@ -0,0 +1,66 @@
+<script>
+	let name = "sebabrata";
+	let job = "developer";
+	let firstName = "nitish";
+	let lastName = "kumar";
+	// funciton
+	// const control = (e)=>{
+	// 	firstName = (e.target.value);
+	// };
+
+	const changeValue = () =>{
+		job = "Manager";
+	};
+
+	// reative value
+	$: fullName = `${firstName} - ${lastName}`;
+
+	// reative statement
+	// $: console.log(firstName);
+
+	// reactive multiple statements
+	$: {
+		console.log(firstName);
+		console.log(lastName);
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>you are a {job}</p>
+	<p>{fullName}</p>
+	<!-- <input type="text" on:input={control} value={job}> -->
+	
+	<!-- two way binding -->
+	<input type="text" bind:value={firstName}>
+	<button on:click={changeValue}>Change</button>
+</main>
+
+<style>
+	button{
+		background:purple;
+		color : white;
+		border : none;
+		padding : 8px 15px;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
